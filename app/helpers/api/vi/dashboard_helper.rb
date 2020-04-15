70e254db7cabd22869b6d49d69f692efcdb55f65
JuniimSI <jrfernandesti@gmail.com>
2020-04-15 13:16:02 -0300

    final da primeira aula

---
+++ app/helpers/api/vi/dashboard_helper.rb
@@ -0,0 +1,2 @@
+module Api::Vi::DashboardHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,31 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: "home#index" 
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', {to: "favorites#create", on: :member}.merge(options)
+      delete '/favorite', {to: "favorites#destroy", on: :member}.merge(options)
+    end
+  end
+
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only: [:show, :index]
+      resources :search, only: :index
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+        
+      end
+      resources :favorites, only: :index
+      resources :songs, only: [] do
+         concerns :favoritable, favoritable_type: 'Song'
+      end 
+    end
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
